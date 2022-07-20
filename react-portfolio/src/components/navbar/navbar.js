17be316d068e1758cf294c8cdaf949748a1cccb8
Grace Marcoux <gmmarcoux@me.com>
2022-07-20 15:33:10 -0500

    im not sure?

--- react-portfolio/src/components/navbar/navbar.js
+++ react-portfolio/src/components/navbar/navbar.js
@@ -1,8 +1,19 @@
 import "./navbar.css";
 import { useContext, useState } from 'react'
+import { resume, resume, contactMe } from '../../portfolio'
 
+const navbar = () => {
+    return {
+        <nav>
+            <ul className="navList">
+                <li>Portfolio</li>
+                <li>Resume</li>
+                <li>Contact Me</li>
+            </ul>
+        </nav
 
-
+    }
+}
 
 
 
